fdb8eaa0c0d8831837f621fe1fdaf7794fd9ae36
Rehok <jambostew@gmail.com>
2016-11-29 09:56:29 +0000

    Create Doom Wolves.lua

---
+++ ElvUI_RehokTags/Doom Wolves.lua
@@ -0,0 +1,10 @@
+function
+
+if strsplit("-"sourceGUID) == "Pet" then
+   if sourceGUID == UnitGUID("pet") then
+      <it was your pet that did damage>
+      
+      
+      224127 -- Lighting Wolf
+      224125 -- Fire Wolf
+      224126 -- Frost Wolf
