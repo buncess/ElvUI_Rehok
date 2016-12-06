2facb9557d56b6b0ed08a5d9f78dbe7efbf27718
Rehok <jambostew@gmail.com>
2016-12-06 09:46:37 +0000

    Update Role-Change.lua

--- ElvUI_RehokTags/Role-Change.lua
+++ ElvUI_RehokTags/Role-Change.lua
@@ -1,35 +1,35 @@
 -- OnInit
 
 aura_env.healspec = { 
-    [105] = true -- Restoration Druid
-    [270] = true -- Mistweaver Monk
-    [65] = true -- Holy Paladin
-    [256] = true -- Discipline Priest
-    [257] = true -- Holy Priest
+    [105] = true, -- Restoration Druid
+    [270] = true, -- Mistweaver Monk
+    [65] = true, -- Holy Paladin
+    [256] = true, -- Discipline Priest
+    [257] = true, -- Holy Priest
     [264] = true -- Restoration Shman
 }
 
 aura_env.damagespec = {
-    [251] = true -- Frost Death Knight
-    [252] = true -- Unholy Death Knight
-    [577] = true -- Havoc Demon Hunter
-    [102] = true -- Balance Druid
-    [103] = true -- Feral Druid
-    [269] = true -- Windwalker Monk
-    [70] = true -- Retribution Paladin
-    [258] = true  -- Shadow Priest
-    [262] = true  -- Elemental Shaman
-    [263] = true  -- Enhancement Shaman
-    [71]  = true -- Arms Warrior
+    [251] = true, -- Frost Death Knight
+    [252] = true, -- Unholy Death Knight
+    [577] = true, -- Havoc Demon Hunter
+    [102] = true, -- Balance Druid
+    [103] = true, -- Feral Druid
+    [269] = true, -- Windwalker Monk
+    [70] = true, -- Retribution Paladin
+    [258] = true,  -- Shadow Priest
+    [262] = true,  -- Elemental Shaman
+    [263] = true, -- Enhancement Shaman
+    [71]  = true, -- Arms Warrior
     [72]  = true -- Fury Warrior
 }
 
 aura_env.tankspec = {
-    [250]  = true -- Blood Death Knight
-    [581 = true  -- Vengeance Demon Hunter
-    [104] = true  -- Guardian Druid
-    [268] = true  -- Brewmaster Monk
-    [66] = true  -- Protection Paladin
-    [73]  = true -- Protection Warrior
+    [250]  = true, -- Blood Death Knight
+    [581 = true, -- Vengeance Demon Hunter
+    [104] = true,  -- Guardian Druid
+    [268] = true,  -- Brewmaster Monk
+    [66] = true,  -- Protection Paladin
+    [73]  = true, -- Protection Warrior
 }
 
